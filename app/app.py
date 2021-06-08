03947e7cd07092db19492c69f6ebab55a7e06e5a
W.R. Jackson <jackson@justjackson.dev>
2021-06-08 16:52:03 -0400

    Working Docker Solution

--- app/app.py
+++ app/app.py
@@ -1,1 +1,14 @@
-# flask server
+'''
+--------------------------------------------------------------------------------
+Description:
+
+Roadmap:
+
+Written by W.R. Jackson <wrjackso@bu.edu>, DAMP Lab 2020
+--------------------------------------------------------------------------------
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+app.run(host="0.0.0.0", port=5000)
