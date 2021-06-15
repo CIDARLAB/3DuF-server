5619e4b01e8febcb15aa65c4b81a5e60b456f2b7
W.R. Jackson <jackson@justjackson.dev>
2021-06-15 17:09:22 -0400

    Addition of FreeCAD to docker build

--- app/app.py
+++ app/app.py
@@ -7,8 +7,18 @@ Roadmap:
 Written by W.R. Jackson <wrjackso@bu.edu>, DAMP Lab 2020
 --------------------------------------------------------------------------------
 """
+import sys
 from flask import Flask
 
+sys.path.append("/usr/lib/freecad-python3/lib")
+import FreeCAD, Draft, Part, Mesh
+
 app = Flask(__name__)
+
+
+@app.route('/echo/<input_string>')
+def echo(input_string: str):
+    return input_string
+
 
 app.run(host="0.0.0.0", port=5000)
